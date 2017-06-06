6b5efbf14e1b452140e757e3257acd7828063aad
gabrielgagz <g.a.gomez@gmail.com>
2017-06-06 11:58:00 -0300

    Remove unused reference in rootdir/Android.mk
    Change-Id: Iadf0c8e884c7624d96f50a569eda63d2dbb983d2

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -81,14 +81,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.sec.boot.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.sec.boot.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.syspart_fixup.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
